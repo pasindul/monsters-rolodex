9a4e3e41e7f2895edb01e53068deca6542e9da57
pasindul <pasindu@ceylonit.com>
2020-02-02 09:56:25 +0530

    Updates

---
+++ precache-manifest.e3baf2e7f8383421fb3ad9c83a9a21ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d67390b88203085cbcf9e33b92e74be",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7ac00bb218d347b1c7d6",
+    "url": "/monsters-rolodex/static/css/main.753bd746.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ac00bb218d347b1c7d6",
+    "url": "/monsters-rolodex/static/js/main.601df3c1.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
